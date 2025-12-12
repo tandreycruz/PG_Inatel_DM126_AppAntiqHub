8348cadef8807465a575edb15df0854133272d82
user288577 <user288577@#13.104>
2025-12-12 17:17:13 -0500

    Commit inicial do projeto AntiqHub

---
+++ App_AntiqHub/App_AntiqHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_AntiqHubApp.swift
+//  App_AntiqHub
+//
+//  Created by user288577 on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct App_AntiqHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
